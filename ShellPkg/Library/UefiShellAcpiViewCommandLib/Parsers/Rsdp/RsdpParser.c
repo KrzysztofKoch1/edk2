73e6d7e117da244f8f4065620115a47f7f66d372
Krzysztof Koch <krzysztof.koch@arm.com>
2019-06-17 13:10:21 +0100

    ShellPkg: acpiview: RSDP: Make printing table checksum optional
    1. Don't validate Root System Description Pointer (RSDP) structure
    checksum if the '-q' command line flag is used with the acpiview UEFI
    shell tool. This change makes the RSDP parser consistent with the
    parsers for other ACPI tables.
    
    2. Check if XsdtAddress pointer has been successfully updated before it
    is used for further table parsing.
    
    3. Remove redundant forward function declarations by repositioning
    blocks of code.
    
    Signed-off-by: Krzysztof Koch <krzysztof.koch@arm.com>

--- ShellPkg/Library/UefiShellAcpiViewCommandLib/Parsers/Rsdp/RsdpParser.c
+++ ShellPkg/Library/UefiShellAcpiViewCommandLib/Parsers/Rsdp/RsdpParser.c
@@ -1,7 +1,7 @@
 /** @file
   RSDP table parser
 
-  Copyright (c) 2016 - 2018, ARM Limited. All rights reserved.
+  Copyright (c) 2016 - 2019, ARM Limited. All rights reserved.
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
   @par Reference(s):
@@ -11,55 +11,10 @@
 #include <Library/UefiLib.h>
 #include "AcpiParser.h"
 #include "AcpiTableParser.h"
+#include "AcpiView.h"
 
 // Local Variables
 STATIC CONST UINT64* XsdtAddress;
-
-/**
-  This function validates the RSDT Address.
-
-  @param [in] Ptr     Pointer to the start of the field data.
-  @param [in] Context Pointer to context specific information e.g. this
-                      could be a pointer to the ACPI table header.
-**/
-STATIC
-VOID
-EFIAPI
-ValidateRsdtAddress (
-  IN UINT8* Ptr,
-  IN VOID*  Context
-  );
-
-/**
-  This function validates the XSDT Address.
-
-  @param [in] Ptr     Pointer to the start of the field data.
-  @param [in] Context Pointer to context specific information e.g. this
-                      could be a pointer to the ACPI table header.
-**/
-STATIC
-VOID
-EFIAPI
-ValidateXsdtAddress (
-  IN UINT8* Ptr,
-  IN VOID*  Context
-  );
-
-/**
-  An array describing the ACPI RSDP Table.
-**/
-STATIC CONST ACPI_PARSER RsdpParser[] = {
-  {L"Signature", 8, 0, NULL, Dump8Chars, NULL, NULL, NULL},
-  {L"Checksum", 1, 8, L"0x%x", NULL, NULL, NULL, NULL},
-  {L"Oem ID", 6, 9, NULL, Dump6Chars, NULL, NULL, NULL},
-  {L"Revision", 1, 15, L"%d", NULL, NULL, NULL, NULL},
-  {L"RSDT Address", 4, 16, L"0x%x", NULL, NULL, ValidateRsdtAddress, NULL},
-  {L"Length", 4, 20, L"%d", NULL, NULL, NULL, NULL},
-  {L"XSDT Address", 8, 24, L"0x%lx", NULL, (VOID**)&XsdtAddress,
-   ValidateXsdtAddress, NULL},
-  {L"Extended Checksum", 1, 32, L"0x%x", NULL, NULL, NULL, NULL},
-  {L"Reserved", 3, 33, L"%x %x %x", Dump3Chars, NULL, NULL, NULL}
-};
 
 /**
   This function validates the RSDT Address.
@@ -132,6 +87,22 @@ ValidateXsdtAddress (
 }
 
 /**
+  An array describing the ACPI RSDP Table.
+**/
+STATIC CONST ACPI_PARSER RsdpParser[] = {
+  {L"Signature", 8, 0, NULL, Dump8Chars, NULL, NULL, NULL},
+  {L"Checksum", 1, 8, L"0x%x", NULL, NULL, NULL, NULL},
+  {L"Oem ID", 6, 9, NULL, Dump6Chars, NULL, NULL, NULL},
+  {L"Revision", 1, 15, L"%d", NULL, NULL, NULL, NULL},
+  {L"RSDT Address", 4, 16, L"0x%x", NULL, NULL, ValidateRsdtAddress, NULL},
+  {L"Length", 4, 20, L"%d", NULL, NULL, NULL, NULL},
+  {L"XSDT Address", 8, 24, L"0x%lx", NULL, (VOID**)&XsdtAddress,
+   ValidateXsdtAddress, NULL},
+  {L"Extended Checksum", 1, 32, L"0x%x", NULL, NULL, NULL, NULL},
+  {L"Reserved", 3, 33, L"%x %x %x", Dump3Chars, NULL, NULL, NULL}
+};
+
+/**
   This function parses the ACPI RSDP table.
 
   This function invokes the parser for the XSDT table.
@@ -156,10 +127,31 @@ ParseAcpiRsdp (
 {
   if (Trace) {
     DumpRaw (Ptr, AcpiTableLength);
-    VerifyChecksum (TRUE, Ptr, AcpiTableLength);
+    if (GetConsistencyChecking ()) {
+      VerifyChecksum (TRUE, Ptr, AcpiTableLength);
+    }
   }
 
-  ParseAcpi (Trace, 0, "RSDP", Ptr, AcpiTableLength, PARSER_PARAMS (RsdpParser));
+  ParseAcpi (
+    Trace,
+    0,
+    "RSDP",
+    Ptr,
+    AcpiTableLength,
+    PARSER_PARAMS (RsdpParser)
+    );
+
+  // Check if the values used to control the parsing logic have been
+  // successfully read.
+  if (XsdtAddress == NULL) {
+    IncrementErrorCount ();
+    Print (
+      L"ERROR: Insufficient table length. AcpiTableLength = %d." \
+        L"RSDP parsing aborted.\n",
+      AcpiTableLength
+      );
+    return;
+  }
 
   // This code currently supports parsing of XSDT table only
   // and does not parse the RSDT table. Platforms provide the
@@ -167,7 +159,7 @@ ParseAcpiRsdp (
   // Therefore the RSDT should not be used on ARM platforms.
   if ((*XsdtAddress) == 0) {
     IncrementErrorCount ();
-    Print (L"ERROR: XSDT Pointer is not set.\n");
+    Print (L"ERROR: XSDT Pointer is not set. RSDP parsing aborted.\n");
     return;
   }
 
