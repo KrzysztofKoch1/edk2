00f5f1bb966aba275842ec3acb77f3ec408c458a
Krzysztof Koch <krzysztof.koch@arm.com>
2020-01-29 13:34:16 +0000

    ShellPkg: acpiview: Remove duplicate ACPI structure size definitions
    Remove duplicate Generic Address Structure (GAS) and ACPI table header
    size definitions. Replace any dependencies with the definitions from
    Acpi63.h.
    
    AcpiParser.h redefines the size of Generic Address Structure (GAS) and
    the ACPI table header. The motivation is to 'minimise any reference to
    the EDK2 ACPI headers in an attempt to provide cross checking'. While
    the redefined values are unlikely to change, redefinition causes
    unnecessary maintenance overhead for acpiview.
    
    In addition to this, EDK2 ACPI headers are referenced throughout the
    entire project, so the assumption should be that the definitions in
    these header files are correct.
    
    Signed-off-by: Krzysztof Koch <krzysztof.koch@arm.com>

--- ShellPkg/Library/UefiShellAcpiViewCommandLib/AcpiParser.c
+++ ShellPkg/Library/UefiShellAcpiViewCommandLib/AcpiParser.c
@@ -1,7 +1,7 @@
 /** @file
   ACPI parser
 
-  Copyright (c) 2016 - 2019, ARM Limited. All rights reserved.
+  Copyright (c) 2016 - 2020, ARM Limited. All rights reserved.
   SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
@@ -666,7 +666,7 @@ DumpGas (
   IN UINT8*        Ptr
   )
 {
-  DumpGasStruct (Ptr, 2, GAS_LENGTH);
+  DumpGasStruct (Ptr, 2, sizeof (EFI_ACPI_6_3_GENERIC_ADDRESS_STRUCTURE));
 }
 
 /**
@@ -687,7 +687,7 @@ DumpAcpiHeader (
            0,
            "ACPI Table Header",
            Ptr,
-           ACPI_DESCRIPTION_HEADER_LENGTH,
+           sizeof (EFI_ACPI_DESCRIPTION_HEADER),
            PARSER_PARAMS (AcpiHeaderParser)
            );
 }
@@ -721,7 +721,7 @@ ParseAcpiHeader (
                   0,
                   NULL,
                   Ptr,
-                  ACPI_DESCRIPTION_HEADER_LENGTH,
+                  sizeof (EFI_ACPI_DESCRIPTION_HEADER),
                   PARSER_PARAMS (AcpiHeaderParser)
                   );
 
